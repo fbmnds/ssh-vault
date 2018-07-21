c47978873c1ab20071446f86f739fc3ad7f34985
fbmnds <fbmnds7@gmail.com>
2018-07-21 20:45:50 +0200

    Generate SHA256 hashes

--- src/Aes256.hs
+++ src/Aes256.hs
@@ -1,24 +1,35 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE GADTs #-}
-module Aes256 () where
+module Aes256 
+  ( 
+      encrypt'
+    , decrypt'
+    , genSHA256
+  ) 
+  where
 
 import           Crypto.Cipher.AES (AES256)
 import           Crypto.Cipher.Types (BlockCipher(..), Cipher(..), nullIV, KeySizeSpecifier(..), IV, makeIV)
+import           Crypto.Hash
+import           Crypto.Hash.Algorithms
 import           Crypto.Error (CryptoFailable(..), CryptoError(..))
 
 import qualified Crypto.Random.Types as CRT
 
 import           Data.ByteArray (ByteArray)
 import           Data.ByteString (ByteString, intercalate)
+import           Data.ByteString.UTF8 (fromString)
+import           Data.Text (Text, unpack)
+
 
 -- | Not required, but most general implementation
 data Key c a where
   Key :: (BlockCipher c, ByteArray a) => a -> Key c a
 
 -- | Generates a string of bytes (key) of a specific length for a given block cipher
-genSecretKey :: forall m c a. (CRT.MonadRandom m, BlockCipher c, ByteArray a) => c -> Int -> m (Key c a)
-genSecretKey _ = fmap Key . CRT.getRandomBytes
+genSecretKey' :: forall m c a. (CRT.MonadRandom m, BlockCipher c, ByteArray a) => c -> Int -> m (Key c a)
+genSecretKey' _ = fmap Key . CRT.getRandomBytes
 
 -- | Generate a random initialization vector for a given block cipher
 genRandomIV :: forall m c. (CRT.MonadRandom m, BlockCipher c) => c -> m (Maybe (IV c))
@@ -32,25 +43,25 @@ initCipher (Key k) = case cipherInit k of
   CryptoFailed e -> Left e
   CryptoPassed a -> Right a
 
-encrypt :: (BlockCipher c, ByteArray a) => Key c a -> IV c -> a -> Either CryptoError a
-encrypt secretKey initIV msg =
+encrypt' :: (BlockCipher c, ByteArray a) => Key c a -> IV c -> a -> Either CryptoError a
+encrypt' secretKey initIV msg =
   case initCipher secretKey of
     Left e -> Left e
     Right c -> Right $ ctrCombine c initIV msg
 
-decrypt :: (BlockCipher c, ByteArray a) => Key c a -> IV c -> a -> Either CryptoError a
-decrypt = encrypt
+decrypt' :: (BlockCipher c, ByteArray a) => Key c a -> IV c -> a -> Either CryptoError a
+decrypt' = encrypt'
 
 exampleAES256 :: ByteString -> IO ()
 exampleAES256 msg = do
   -- secret key needs 256 bits (32 * 8)
-  secretKey <- genSecretKey (undefined :: AES256) 32
+  secretKey <- genSecretKey' (undefined :: AES256) 32
   mInitIV <- genRandomIV (undefined :: AES256)
   case mInitIV of
     Nothing -> error "Failed to generate and initialization vector."
     Just initIV -> do
-      let encryptedMsg = encrypt secretKey initIV msg
-          decryptedMsg = decrypt secretKey initIV =<< encryptedMsg
+      let encryptedMsg = encrypt' secretKey initIV msg
+          decryptedMsg = decrypt' secretKey initIV =<< encryptedMsg
       case (,) <$> encryptedMsg <*> decryptedMsg of
         Left err -> error $ show err
         Right (eMsg, dMsg) -> do
@@ -62,7 +73,16 @@ exampleAES256 msg = do
 mainAes :: IO ()
 mainAes = 
   exampleAES256 $ 
-  intercalate "\n" [ 
+  Data.ByteString.intercalate "\n" [ 
       "This is some text which we escape" 
     , "and unescape to keep writing" 
     ]
+
+
+genSHA256 :: Text -> Digest SHA256
+genSHA256 key = 
+  hash . Prelude.head $ fmap Data.ByteString.UTF8.fromString [Data.Text.unpack key] 
+
+
+encrypt :: Text -> Text -> Either CryptoError Text
+encrypt key msg = undefined
--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,9 @@
 module Main where
 
 import Ssh (ssh)
+import Aes256 (genSHA256)
 import Turtle
 
-main :: IO ExitCode
-main = ssh
+main :: IO () --ExitCode
+main = 
+ printf w. genSHA256 $ fromString "123\n"
